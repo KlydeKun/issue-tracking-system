e7fe915a9a4d60c3d0e5e08ac79e99324f22558c
Kyle Lerio <kylelerio15@gmail.com>
2024-07-03 10:41:24 +0800

    Resolve the server issue (500) error

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -25,24 +25,13 @@ const NewIssuePage = () => {
     >
       <TextField.Root placeholder="Title" {...register("title")} />
 
-      {/* TODO:
-       Controller is the cause of having GET request server error */}
-      {/* <Controller
+      <Controller
         name="description"
         control={control}
-        // render={({ field }) => (
-        //   <SimpleMDE placeholder="Description" {...field} />
-        // )}
         render={({ field }) => (
-          <SimpleMDE
-            {...field}
-            onChange={(value) => field.onChange(value)}
-            options={{
-              placeholder: "Description",
-            }}
-          />
+          <SimpleMDE placeholder="Description" {...field} />
         )}
-      /> */}
+      />
       <Button>Submit New Issue</Button>
     </form>
   );
