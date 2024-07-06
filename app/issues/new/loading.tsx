7fdf4d0f152f360e28647f514f5f5ca805f51983
KlydeKun <kylelerio15@gmail.com>
2024-07-07 00:36:35 +0800

    Update the skeleton of the New issue loading

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,17 +1,14 @@
-import { Box } from "@radix-ui/themes";
-import React from "react";
+import { Box, TextArea, TextField } from "@radix-ui/themes";
+import React, { Suspense } from "react";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
 const LoadingNewIssuePage = () => {
   return (
-    // <Box className="max-w-xl">
-    //   <Skeleton />
-    //   <Skeleton className="h-[20rem]" />
-    // </Box>
-    <div>
-      <h1>Loading...</h1>
-    </div>
+    <Box className="max-w-xl">
+      <Skeleton />
+      <Skeleton className="h-[20rem]" />
+    </Box>
   );
 };
 
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,11 +1,7 @@
-import React from "react";
-import { Table } from "@radix-ui/themes";
-
 import prisma from "@/prisma/client";
-import IssueStatusBadge from "../components/IssueStatusBadge";
+import { Table } from "@radix-ui/themes";
+import { IssueStatusBadge, Link } from "@/app/components";
 import IssuesActions from "./IssuesActions";
-import delay from "delay";
-import Link from "../components/Link";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
