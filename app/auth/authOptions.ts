6741d6c40464f0e6d5ccdc0407878963c9ab17a1
KlydeKun <kylelerio15@gmail.com>
2024-07-16 00:08:58 +0800

    Undo some changes

--- app/auth/authOptions.ts
+++ app/auth/authOptions.ts
@@ -5,12 +5,12 @@ import { NextAuthOptions } from "next-auth";
 
 const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
-  // providers: [
-  //   GoogleProvider({
-  //     clientId: process.env.GOOGLE_CLIENT_ID!,
-  //     clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-  //   }),
-  // ],
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
   session: {
     strategy: "jwt",
   },
