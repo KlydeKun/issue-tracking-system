004d1553436f31c314a67ee6cebe6d0bf3f64c8e
KlydeKun <kylelerio15@gmail.com>
2024-07-22 23:15:21 +0800

    Modify the issueStatusFilter component

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -1,25 +1,31 @@
 "use client";
+
 import { Status } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
+import { useRouter } from "next/navigation";
 import React from "react";
 
-const statuses: { label: string; value?: Status }[] = [
-  { label: "All" },
+const statuses: { label: string; value: Status | "ALL" }[] = [
+  { label: "All", value: "ALL" },
   { label: "Open", value: "OPEN" },
   { label: "In Progress", value: "IN_PROGRESS" },
   { label: "Close", value: "CLOSED" },
 ];
 
 const IssueStatusFilter = () => {
+  const router = useRouter();
+
   return (
-    <Select.Root>
+    <Select.Root
+      onValueChange={(status) => {
+        const query = status && status !== "ALL" ? `?status=${status}` : "";
+        router.push("/issues/list" + query);
+      }}
+    >
       <Select.Trigger placeholder="Filter by status..." />
       <Select.Content>
         {statuses.map((status) => (
-          <Select.Item
-            key={status.value ?? "ALL"}
-            value={status.value ?? "ALL"}
-          >
+          <Select.Item key={status.value} value={status.value}>
             {status.label}
           </Select.Item>
         ))}
--- app/layout.tsx
+++ app/layout.tsx
@@ -20,13 +20,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
       <QueryClientProvider>
-        <body className={inter.variable}>
+        <body className={inter.variable} >
           <AuthProvider>
             <Theme accentColor="violet" appearance="light">
               <NavBar />
